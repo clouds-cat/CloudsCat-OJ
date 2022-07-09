44598a0f7a9ad40e0b2d205b18ceed143a0133c7
xlzzzz <13960959677>
2022-07-09 18:21:09 +0800

    docs test3

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,7 @@ module.exports = {
         lineNumbers: true // 代码块显示行号
     },
     themeConfig: {
-        sidebarDepth: 3,
+        sidebarDepth: 2,
         nav: [
             { text: 'Demo', link: '/' },
             { text: 'GitHub首页', link: 'https://github.com/clouds-cat/' },
