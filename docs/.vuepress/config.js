dbc3197532b99a41b8a7a4b889a1aa61ff790603
gsjztle <489396184@qq.com>
2022-07-09 19:13:39 +0800

    docs test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,17 +8,44 @@ module.exports = {
     markdown: {
         lineNumbers: true ,// 代码块显示行号
         toc: {
-            includeLevel:[1, 2, 3, 4]
+            includeLevel:[1, 2]
         }
     },
     themeConfig: {
         sidebarDepth: 2,
-
         nav: [
             { text: 'Demo', link: '/' },
             { text: 'GitHub首页', link: 'https://github.com/clouds-cat/' },
             { text: '团队首页', link: 'https://github.com/clouds-cat/' },
         ],
-        sidebar: 'auto'
+        sidebar: [
+            {
+                title: '开始上手',
+                collapsable: true,
+                children: [
+                    '/faq/',
+                ]
+            },
+            {
+                title: '部署文档',
+                collapsable: true,
+            },
+            {
+                title: '开发文档',
+                collapsable: true,
+                children: [
+                    '/dev/',
+                    '/dev/database',
+                ]
+            },
+            {
+                title: '使用文档',
+                collapsable: true,
+                children:[
+                    '/use/',
+                    '/use/judge',
+                ]
+            },
+        ]
     },
 }
